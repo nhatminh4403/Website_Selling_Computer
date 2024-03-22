a63ae40ce9cba70d3635e0ef052e9721011e15ce
NLSon03 <nlson.2003.nguyen@gmail.com>
2024-03-22 19:59:49 +0700

    Add project files.

---
+++ Website_Selling_Computer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Website_Selling_Computer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
